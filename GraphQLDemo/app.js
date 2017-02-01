d05c18ef67caf106aa43a6c3007d1d0e35c4ecf7
Galen Caldwell <gcc17@case.edu>
2017-01-31 22:58:10 -0500

    Added comments
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

--- GraphQLDemo/app.js
+++ GraphQLDemo/app.js
@@ -13,6 +13,15 @@ var app = express();
 
 let count = 0;
 
+/* You can run this query in the GraphIQL explorer with any combination
+    of the following fields:
+
+*  {
+*      count
+*      author
+*  }
+
+*/
 let myGraphQLSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
@@ -31,6 +40,13 @@ let myGraphQLSchema = new GraphQLSchema({
       }
     }
   }),
+  /* You can call this mutation in the GraphIQL explorer with:
+
+  *  mutation {
+  *      updateCount
+  *  }
+
+  */
   mutation: new GraphQLObjectType({
     name: 'RootMutationType',
     fields: {
@@ -46,16 +62,21 @@ let myGraphQLSchema = new GraphQLSchema({
   })
 })
 
+// A response to a simple root level get request
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+// Adds an endpoint for our graphql server
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
 
+// Sets up a graphiql endpoint to explore and test our graphql server. Calls
+//  original graphql endpoint for data.
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
+// Sets the server to listen at the given port
 app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 })
