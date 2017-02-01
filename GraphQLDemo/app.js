40313a34ce8f5784246f0e94210b7cb7063448cf
Galen Caldwell <gcc17@case.edu>
2017-01-31 22:50:41 -0500

    Added query with int and string fields, no nesting
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

---
+++ GraphQLDemo/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString
+} from 'graphql';
+
+const PORT = 3000;
+var app = express();
+
+let count = 0;
+
+let myGraphQLSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      count: {
+        type: GraphQLInt,
+        resolve: function() {
+          return count;
+        }
+      },
+      author: {
+        type: GraphQLString,
+        resolve: function() {
+          return "Mitchell Baller"
+        }
+      }
+    }
+  })
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port 3000!')
+})
