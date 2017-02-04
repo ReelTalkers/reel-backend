c071737084f756e5c97ad244913ad99e5ae3d71b
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-02-04 15:58:31 -0500

    Add connectors using sqlite.

---
+++ data/connectors.js
@@ -0,0 +1,30 @@
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+
+const db = new Sequelize('database', null, null, {
+  dialect: 'sqlite',
+  storage: './databse.sqlite'
+});
+
+const User = db.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+  },
+  firstName: {
+    type: Sequelize.STRING,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+  },
+});
+
+User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    userName: 'johncock',
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+export { User };
