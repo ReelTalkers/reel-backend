3d473c052fd49b2edf9986af0d05e034a7005c94
Galen Caldwell <gcc17@case.edu>
2017-03-06 12:26:58 -0500

    Fixed typo
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

--- data/connectors.js
+++ data/connectors.js
@@ -157,10 +157,10 @@ const Movie = {
 }
 
 casual.seed(123);
-User.sync({ options.logging=console.log });
+User.sync({ logging=console.log });
 
-Person.sync({ options.logging=console.log });
+Person.sync({ logging=console.log });
 
-Media.sync({ options.logging=console.log });
+Media.sync({ logging=console.log });
 
 export { User, Person, Media, Movie };
