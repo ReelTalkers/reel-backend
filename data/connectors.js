951693d7d4a698397468071e76c5d164f65e9d89
Galen Caldwell <gcc17@case.edu>
2017-03-07 15:34:57 -0500

    Fixed another merge conflict
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

--- data/connectors.js
+++ data/connectors.js
@@ -164,10 +164,6 @@ User.sync({ logging:console.log });
 
 Person.sync({ logging:console.log });
 
-<<<<<<< HEAD
 Media.sync({ logging:console.log });
-=======
-Media.sync({ force: true });
->>>>>>> media
 
 export { User, Person, Media };
